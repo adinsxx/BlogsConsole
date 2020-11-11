40a8433e21d86e507cd0cc67544c6184c234c259
Connor <c.mccurrie@gmail.com>
2020-11-11 13:43:40 -0600

    added options to add/view data

--- Program.cs
+++ Program.cs
@@ -14,25 +14,59 @@ namespace BlogsConsole
             logger.Info("Program started");
             try
             {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
+                Console.WriteLine("Enter your selection");
+                Console.WriteLine("1) Display all blogs");
+                Console.WriteLine("2) Add Blog");
+                Console.WriteLine("3) Create Post");
+                Console.WriteLine("4) Display Posts");
+                Console.WriteLine("Enter q to quit");
+                var choice = Console.ReadLine();
                 var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
 
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                if (choice == "1")
                 {
-                    Console.WriteLine(item.Name);
+                    logger.Info("Option 1 selected");
+                    // Display all Blogs from the database
+                    var query = db.Blogs.OrderBy(b => b.Name);
+
+                    if (query != null)
+                    {
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var blogName in query)
+                        {
+                            Console.WriteLine(blogName.Name);
+                        }
+                    }
+                    else
+                    {
+                        {
+                            Console.WriteLine("No blogs available");
+                        }
+
+                    }
                 }
+                else if (choice == "2")
+                {
+                    // Create and save a new Blog
+                    Console.Write("Enter a name for a new Blog: ");
+                    var name = Console.ReadLine();
+                    var blog = new Blog { Name = name };
+                    db.AddBlog(blog);
+                    logger.Info("Blog added - {name}", name);
+
+                }
+                else if (choice == "3")
+                {
+                    Console.WriteLine(":" );
+                    var title = Console.ReadLine();
+                    var postTitle = new Post {Title = title};
+                    
+                }
+                else if (choice == "4")
+                {
+
+                }
+
             }
             catch (Exception ex)
             {
